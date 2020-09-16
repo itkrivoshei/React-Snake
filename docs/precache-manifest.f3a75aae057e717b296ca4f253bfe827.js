70ceb4df366aad2d222d833c0eb07a1ae9fe6d0a
Gitshey <krivoshey.nikita1998@gmail.com>
2020-09-16 17:18:44 +0300

    GHP fix

---
+++ docs/precache-manifest.f3a75aae057e717b296ca4f253bfe827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fabca4c1b3d525f101f001b733c1ae0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d89bfb154e9b0d4bcbd",
+    "url": "./static/css/main.221d8c2d.chunk.css"
+  },
+  {
+    "revision": "c397f79567929733cec4",
+    "url": "./static/js/2.85a31954.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.85a31954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d89bfb154e9b0d4bcbd",
+    "url": "./static/js/main.b4f3da87.chunk.js"
+  },
+  {
+    "revision": "0980d2052843cbec2da2",
+    "url": "./static/js/runtime-main.0c2b6142.js"
+  }
+]);
